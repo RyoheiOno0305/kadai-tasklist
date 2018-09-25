fadcf2893471b2c08dfcdcea2c8c852c5f9ae751
EC2 Default User <ec2-user@ip-172-31-95-221.ec2.internal>
2018-09-25 09:24:41 +0000

    add_column

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,10 +1,12 @@
 class TasksController < ApplicationController
+  before_action :set_task, only: [:show, :edit, :update, :destroy]
+  
   def index
      @tasks = Task.all
   end
 
   def show
-      @task = Task.find(params[:id])
+      set_task
   end
 
   def new
@@ -26,11 +28,11 @@ class TasksController < ApplicationController
   end
 
   def edit
-      @task = Task.find(params[:id])
+      set_task
   end
 
   def update
-       @task = Task.find(params[:id])
+       set_task
 
     if @task.update(task_params)
       flash[:success] = 'Task は正常に更新されました'
@@ -40,12 +42,24 @@ class TasksController < ApplicationController
       render :edit
     end
   end
+    
+  
+  def destroy
+    set_task
+    @task.destroy
+
+    flash[:success] = 'Task は正常に削除されました'
+    redirect_to tasks_url
+  end
   
   private
 
   # Strong Parameter
   def task_params
-    params.require(:task).permit(:content)
+    params.require(:task).permit(:content, :status)
   end
-
+  
+  def set_task
+    @task = Task.find(params[:id])
+  end
 end
