960820dbe51b4d126ed35e5a68e9c0a922226a0d
EC2 Default User <ec2-user@ip-172-31-3-113.ec2.internal>
2018-09-30 04:05:43 +0000

    post

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,22 @@
 Rails.application.routes.draw do
+  
+  get 'microposts/create'
+
+  get 'microposts/destroy'
+
+  get 'toppages/index'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root to: 'tasks#index'
+  root to: 'toppages#index'
+  
+  get 'login' => 'sessions#new'
+  post 'login' => 'sessions#create'
+  delete'logout' =>'sessions#destroy'
+  
+  get 'signup' => 'users#new'
+  resources :users, only: [:index, :show, :new, :create]
+  
+  resources :microposts, only: [:create, :destroy]
 
   resources :tasks
 end
